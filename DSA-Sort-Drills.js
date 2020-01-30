3c6e0661f32e293c2664da6bf09e0cb63fe50816
Michael Weedman <michael.weedman@gmail.com>
2020-01-30 10:47:41 -0700

    Completed 4.

--- DSA-Sort-Drills.js
+++ DSA-Sort-Drills.js
@@ -1,9 +1,48 @@
-// 1
+const sort = require('./sort');
 
-//a) 1, 21, 26, 45 | 29, 28, 2, 9 | 16, 49, 39, 27, 43, 34, 46, 40
+/* 1 - Understanding merge sort
 
-//b) 1, 2, 9, 16, 21, 26, 27, 28, 29, 34, 39, 40, 43, 45, 46, 49
+  a) 1, 21, 26, 45 | 29, 28, 2, 9 | 16, 49, 39, 27, 43, 34, 46, 40
 
-//c) [21], [1]
+  b) 1, 2, 9, 16, 21, 26, 27, 28, 29, 34, 39, 40, 43, 45, 46, 49
 
-//d) [1, 21, 26, 45], [2, 9, 28, 29]
+  c) [21], [1]
+
+  d) [1, 21, 26, 45], [2, 9, 28, 29]
+
+
+  2 - Understanding quicksort
+
+  after first partition: 
+  [3, 9, 1, 14, 17, 24, 22, 20]
+
+  1) The pivot could have been either 14 or 17 because the array is arranged in a correct order around them: everything to the left of those indices are less than the value, and everything to the right of them is greater than the value.
+
+  2) Given: [14, 17, 13, 15, 19, 10, 3, 16, 9, 12]
+    shoe the resulting list after second partitioning. 
+
+    a) when using last item as pivot:
+      1st: [[10, 3, 9], [12], [19, 14, 17, 16, 13, 15]]
+      2nd: [[3],[9],[10],[12],[19, 14, 17, 16, 13, 15]]
+
+    b) when using first item as pivot:
+      1st: [[12,13,10,3,9],[14],[15,16,19,17]]
+      2nd: [[[9,10,3],[12],[13]],[14],[15,16,19,17]]
+  
+*/
+
+//3 - Implementing quicksort
+const testData = [89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 8, 7, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5];
+
+
+console.log('Quick: ', sort.quickSort(testData));
+
+//4 - Implementing merge sort 
+
+//SEE sort.js mergeSort function.
+
+console.log('Merge: ', sort.mergeSort(testData));
+
+//5 - Sorting a linked list using merge sort
+
+
--- sort.js
+++ sort.js
@@ -1,88 +1,88 @@
 //// Bubble Sort ////
-
-function swap(array, i, j) {
-  const tmp = array[i];
-  array[i] = array[j];
-  array[j] = tmp;
-}
-
-function bubbleSort(array) {
-  let swaps = 0;
-  for (let i = 0; i < array.length -1; i++) {
-    if (array[i] > array[i + 1]) {
-      swap(array, i, i + 1);
-      swaps++;
+const sorts = {
+  swap: function(array, i, j) {
+    const tmp = array[i];
+    array[i] = array[j];
+    array[j] = tmp;
+  },
+  
+  bubbleSort: function(array) {
+    let swaps = 0;
+    for (let i = 0; i < array.length -1; i++) {
+      if (array[i] > array[i + 1]) {
+        this.swap(array, i, i + 1);
+        swaps++;
+      }
     }
-  }
-  if (swaps > 0) {
-    return bubbleSort(array);
-  }
-  return array;
-}
-
-
-//// Merge Sort ////
-
-function mergeSort(array) {
-  if (array.length <= 1) {
+    if (swaps > 0) {
+      return this.bubbleSort(array);
+    }
     return array;
-  }
-
-  const middle = Math.floor(array.length / 2);
-  let left = array.slice(0, middle);
-  let right = array.slice(middle, array.length);
-
-  left = mergeSort(left);
-  right = mergeSort(right);
-  return merge(left, right, array);
-}
-
-function merge(left, right, array) {
-  let leftIndex = 0;
-  let rightIndex = 0;
-  let outputIndex = 0;
-  while(leftIndex < left.length && rightIndex < right.length) {
-    if(left[leftIndex] < right[rightIndex]) {
-      array[outputIndex++] = left[leftIndex++];
+  },
+  
+  
+  //// Merge Sort ////
+  
+  mergeSort: function(array) {
+    if (array.length <= 1) {
+      return array;
     }
-    else {
-      array[outputIndex++] = right[rightIndex++];
+  
+    const middle = Math.floor(array.length / 2);
+    let left = array.slice(0, middle);
+    let right = array.slice(middle, array.length);
+  
+    left = this.mergeSort(left);
+    right = this.mergeSort(right);
+    return this.merge(left, right, array);
+  },
+  
+  merge: function(left, right, array) {
+    let leftIndex = 0;
+    let rightIndex = 0;
+    let outputIndex = 0;
+    while(leftIndex < left.length && rightIndex < right.length) {
+      if(left[leftIndex] < right[rightIndex]) {
+        array[outputIndex++] = left[leftIndex++];
+      }
+      else {
+        array[outputIndex++] = right[rightIndex++];
+      }
     }
-  }
-  for (let i = leftIndex; i < left.length; i++) {
-    array[outputIndex++] = left[i];
-  }
-  for (let i =rightIndex; i < right.length; i++) {
-    array[outputIndex++] = right[i];
-  }
-  return array;
-}
-
-
-//// QUICKSORT ////
-
-function quickSort(array, start = 0, end = array.length) {
-  if (start >= end) {
+    for (let i = leftIndex; i < left.length; i++) {
+      array[outputIndex++] = left[i];
+    }
+    for (let i =rightIndex; i < right.length; i++) {
+      array[outputIndex++] = right[i];
+    }
     return array;
-  }
-  const middle = partition(array, start, end);
-  array = quickSort(array, start, middle);
-  array = quickSort(array, middle + 1, end);
-  return array;
-}
-
-function partition(array, start, end) {
-  const pivot = array[end - 1];
-  let j = start;
-  for (let i = start; i < end -1; i++) {
-    if (array[i] <= pivot) {
-      swap(array, i, j);
-      j++;
+  },
+  
+  
+  //// QUICKSORT ////
+  
+  quickSort: function(array, start = 0, end = array.length) {
+    if (start >= end) {
+      return array;
     }
+    const middle = this.partition(array, start, end);
+    array = this.quickSort(array, start, middle);
+    array = this.quickSort(array, middle + 1, end);
+    return array;
+  },
+  
+  partition: function(array, start, end) {
+    const pivot = array[end - 1];
+    let j = start;
+    for (let i = start; i < end -1; i++) {
+      if (array[i] <= pivot) {
+        this.swap(array, i, j);
+        j++;
+      }
+    }
+    this.swap(array, end - 1, j);
+    return j;
   }
-  swap(array, end - 1, j);
-  return j;
-}
+};
 
-
-//1 - Understanding Merge Sort
+module.exports = sorts;
